5647f1ecdd6066631f10a88135ac9e897f0ae169
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-02 13:07:52 +0100

    Results page, pagination

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,5 +1,5 @@
 // src/pages/Home.jsx
-import React from "react";
+import React, { useState} from "react";
 
 const Results = () => {
   const data = [
@@ -37,13 +37,148 @@ const Results = () => {
       month: "March",
       averageReturn: 1451,
       averageAccuracy: 78,
+      trades: [
+        {
+          pair: "DOT/USDT",
+          lev: "5X",
+          profit: "100",
+        },
+        {
+          pair: "ADA/USDT",
+          lev: "5X",
+          profit: "100",
+        },
+        {
+          pair: "LTC/USDT",
+          lev: "5X",
+          profit: "200",
+        },
+        {
+          pair: "NEO/USDT",
+          lev: "5X",
+          profit: "25",
+        },
+        {
+          pair: "RLC/USDT",
+          lev: "5X",
+          profit: "100",
+        },
+        {
+          pair: "BTC/USDT",
+          lev: "5X",
+          profit: "100",
+        },
+        {
+          pair: "ETH/USDT",
+          lev: "5X",
+          profit: "75",
+        },
+        {
+          pair: "SOL/USDT",
+          lev: "5X",
+          profit: "50",
+        },
+        {
+          pair: "BTC/USDT",
+          lev: "5X",
+          profit: "90",
+        },
+        {
+          pair: "ETH/USDT",
+          lev: "5X",
+          profit: "75",
+        },
+        {
+          pair: "LTC/USDT",
+          lev: "5X",
+          profit: "100",
+        },
+        {
+          pair: "FET/USDT",
+          lev: "5X",
+          profit: "100",
+        },
+        {
+          pair: "FXS/USDT",
+          lev: "5X",
+          profit: "100",
+        },
+        {
+          pair: "OP/USDT",
+          lev: "5X",
+          profit: "100",
+        },
+        {
+          pair: "ICX/USDT",
+          lev: "5X",
+          profit: "75",
+        },
+        {
+          pair: "SOL/USDT",
+          lev: "5X",
+          profit: "75",
+        },
+        {
+          pair: "SHIB/USDT",
+          lev: "5X",
+          profit: "1",
+        },
+        {
+          pair: "ETH/USDT",
+          lev: "5X",
+          profit: "75",
+        },        
+        {
+          pair: "LDO/USDT",
+          lev: "5X",
+          profit: "75",
+        },
+        {
+          pair: "SOL/USDT",
+          lev: "5X",
+          profit: "25",
+        },
+        {
+          pair: "SHIB/USDT",
+          lev: "SPOT",
+          profit: "10",
+        },
+        {
+          pair: "ETH/USDT",
+          lev: "5X",
+          profit: "-30",
+        },
+        {
+          pair: "SOL/USDT",
+          lev: "5X",
+          profit: "-50",
+        },
+        {
+          pair: "BTC/USDT",
+          lev: "5X",
+          profit: "-20",
+        },
+        {
+          pair: "BTC/USDT",
+          lev: "5X",
+          profit: "-25",
+        },
+        {
+          pair: "BNB/USDT",
+          lev: "5X",
+          profit: "-25",
+        },
+        {
+          pair: "BCH/USDT",
+          lev: "5X",
+          profit: "-25",
+        },
+      ]
     },
     {
       id: 6,
       year: 2023,
       month: "April",
-      averageReturn: 687,
-      averageAccuracy: 81,
       trades: [
         {
           pair: "INJ/USDT",
@@ -129,6 +264,9 @@ const Results = () => {
     },
   ];
 
+  const cardsPerPage = 3;
+  const [currentPage, setCurrentPage] = useState(1);
+
   const reversedData = [...data].reverse();
 
   const calculateAverage = (key) => {
@@ -152,35 +290,50 @@ const Results = () => {
       return 0;
     });
   };
+
+  const totalPages = Math.ceil(reversedData.length / cardsPerPage);
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const startIdx = (currentPage - 1) * cardsPerPage;
+  const endIdx = startIdx + cardsPerPage;
+
+  const visibleData = reversedData.slice(startIdx, endIdx);
   
   return (
     <div className="bg-main bg-cover font-Montserrat">
-    <div className="container max-w-5xl mx-auto py-16">
-      <h1 className="text-5xl md:text-5xl lg:text-6xl font-bold tracking-widest mb-8 text-center text-white">
-        Monthly <span className="text-light-blue">Trade</span> Reports
-      </h1>
-      <p className="text-center mb-8 uppercase text-light-gray font-medium tracking-widest">
-        Every trade we have every took
-      </p>
-      <div className="grid grid-cols-1 grid-flow-row gap-4 max-w-3xl mx-auto">
-        {reversedData.map((item, index) => (
-          <div
-            key={item.id}
-            className="p-4 border border-white border-opacity-10 rounded-lg bg-[#fff] bg-opacity-10 backdrop-blur-[20px]"
-          >
-            <div className="mb-4 flex justify-between">
-              <h1 className="text-4xl font-bold text-white">
-                <span className="text-yellow">{`${item.month}`}</span>{" "}
-                {`${item.year}`}
-              </h1>
-              <div>
-                <ul className="text-light-gray text-right">
-                  <li>Signal Gains: {calculateAverage("return")[index].toFixed(0)}%</li>
-                  <li>Accuracy: {calculateAverage("accuracy")[index].toFixed(0)}%</li>
-                </ul>
+      <div className="container max-w-5xl mx-auto py-16">
+        <h1 className="text-5xl md:text-5xl lg:text-6xl font-bold tracking-widest mb-8 text-center text-white">
+          Monthly <span className="text-light-blue">Trade</span> Reports
+        </h1>
+        <p className="text-center mb-8 uppercase text-light-gray font-medium tracking-widest">
+          Every trade we have every took
+        </p>
+        <div className="grid grid-cols-1 grid-flow-row gap-4 max-w-3xl mx-auto">
+          {visibleData.map((item, index) => (
+            <div
+              key={item.id}
+              className="p-4 border border-white border-opacity-10 rounded-lg bg-[#fff] bg-opacity-10 backdrop-blur-[20px]"
+            >
+              <div className="mb-4 flex justify-between">
+                <h1 className="text-4xl font-bold text-white">
+                  <span className="text-yellow">{`${item.month}`}</span>{" "}
+                  {`${item.year}`}
+                </h1>
+                <div>
+                  <ul className="text-light-gray text-right">
+                    <li>Signal Gains: {calculateAverage("return")[index].toFixed(0)}%</li>
+                    <li>Accuracy: {calculateAverage("accuracy")[index].toFixed(0)}%</li>
+                  </ul>
+                </div>
               </div>
-            </div>
-            <div>
+              <div>
                 <table className="table-auto w-full">
                   <thead>
                     <tr>
@@ -206,6 +359,25 @@ const Results = () => {
             </div>
           ))}
         </div>
+      </div>
+      <div className="flex justify-center items-center mt-4">
+        <button
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+          className="bg-light-blue text-white font-bold py-2 px-4 rounded"
+        >
+          Previous Page
+        </button>
+        <p className="text-white mx-8">
+          Page {currentPage} / {totalPages}
+        </p>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className="bg-light-blue text-white font-bold py-2 px-4 rounded"
+        >
+          Next Page
+        </button>
       </div>
     </div>
   );
