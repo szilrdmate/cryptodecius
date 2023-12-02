82ccb3c107e7535f2597ac2af5ccb5c6a3ab3a54
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-02 15:39:23 +0100

    Completer trade hsitory

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,296 +1,12 @@
 // src/pages/Home.jsx
 import React, { useState} from "react";
+import data from "../data.json";
 
 const Results = () => {
-  const data = [
-    {
-      id: 1,
-      year: 2022,
-      month: "November",
-      averageReturn: 3075,
-      averageAccuracy: 82,
-    },
-    {
-      id: 2,
-      year: 2022,
-      month: "December",
-      averageReturn: 1665,
-      averageAccuracy: 82,
-    },
-    {
-      id: 3,
-      year: 2023,
-      month: "January",
-      averageReturn: 2160,
-      averageAccuracy: 80,
-    },
-    {
-      id: 4,
-      year: 2023,
-      month: "February",
-      averageReturn: 1420,
-      averageAccuracy: 80,
-    },
-    {
-      id: 5,
-      year: 2023,
-      month: "March",
-      averageReturn: 1451,
-      averageAccuracy: 78,
-      trades: [
-        {
-          pair: "DOT/USDT",
-          lev: "5X",
-          profit: "100",
-        },
-        {
-          pair: "ADA/USDT",
-          lev: "5X",
-          profit: "100",
-        },
-        {
-          pair: "LTC/USDT",
-          lev: "5X",
-          profit: "200",
-        },
-        {
-          pair: "NEO/USDT",
-          lev: "5X",
-          profit: "25",
-        },
-        {
-          pair: "RLC/USDT",
-          lev: "5X",
-          profit: "100",
-        },
-        {
-          pair: "BTC/USDT",
-          lev: "5X",
-          profit: "100",
-        },
-        {
-          pair: "ETH/USDT",
-          lev: "5X",
-          profit: "75",
-        },
-        {
-          pair: "SOL/USDT",
-          lev: "5X",
-          profit: "50",
-        },
-        {
-          pair: "BTC/USDT",
-          lev: "5X",
-          profit: "90",
-        },
-        {
-          pair: "ETH/USDT",
-          lev: "5X",
-          profit: "75",
-        },
-        {
-          pair: "LTC/USDT",
-          lev: "5X",
-          profit: "100",
-        },
-        {
-          pair: "FET/USDT",
-          lev: "5X",
-          profit: "100",
-        },
-        {
-          pair: "FXS/USDT",
-          lev: "5X",
-          profit: "100",
-        },
-        {
-          pair: "OP/USDT",
-          lev: "5X",
-          profit: "100",
-        },
-        {
-          pair: "ICX/USDT",
-          lev: "5X",
-          profit: "75",
-        },
-        {
-          pair: "SOL/USDT",
-          lev: "5X",
-          profit: "75",
-        },
-        {
-          pair: "SHIB/USDT",
-          lev: "5X",
-          profit: "1",
-        },
-        {
-          pair: "ETH/USDT",
-          lev: "5X",
-          profit: "75",
-        },        
-        {
-          pair: "LDO/USDT",
-          lev: "5X",
-          profit: "75",
-        },
-        {
-          pair: "SOL/USDT",
-          lev: "5X",
-          profit: "25",
-        },
-        {
-          pair: "SHIB/USDT",
-          lev: "SPOT",
-          profit: "10",
-        },
-        {
-          pair: "ETH/USDT",
-          lev: "5X",
-          profit: "-30",
-        },
-        {
-          pair: "SOL/USDT",
-          lev: "5X",
-          profit: "-50",
-        },
-        {
-          pair: "BTC/USDT",
-          lev: "5X",
-          profit: "-20",
-        },
-        {
-          pair: "BTC/USDT",
-          lev: "5X",
-          profit: "-25",
-        },
-        {
-          pair: "BNB/USDT",
-          lev: "5X",
-          profit: "-25",
-        },
-        {
-          pair: "BCH/USDT",
-          lev: "5X",
-          profit: "-25",
-        },
-      ]
-    },
-    {
-      id: 6,
-      year: 2023,
-      month: "April",
-      trades: [
-        {
-          pair: "INJ/USDT",
-          lev: "5X",
-          profit: "75",
-        },
-        {
-          pair: "LDO/USDT",
-          lev: "5X",
-          profit: "25",
-        },
-        {
-          pair: "SOL/USDT",
-          lev: "5X",
-          profit: "110",
-        },
-        {
-          pair: "BNB/USDT",
-          lev: "5X",
-          profit: "35",
-        },
-        {
-          pair: "ROSE/USDT",
-          lev: "5X",
-          profit: "135",
-        },
-        {
-          pair: "INJ/USDT",
-          lev: "5X",
-          profit: "60",
-        },
-        {
-          pair: "FXS/USDT",
-          lev: "5X",
-          profit: "70",
-        },
-        {
-          pair: "GPT/USDT",
-          lev: "10X",
-          profit: "200",
-        },
-        {
-          pair: "SHIB/USDT",
-          lev: "SPOT",
-          profit: "10",
-        },
-        {
-          pair: "QI/USDT",
-          lev: "SPOT",
-          profit: "7",
-        },
-        {
-          pair: "UNI/USDT",
-          lev: "SPOT",
-          profit: "5",
-        },
-        {
-          pair: "SOL/USDT",
-          lev: "HODL",
-          profit: "20",
-        },
-        {
-          pair: "BNB/USDT",
-          lev: "HODL",
-          profit: "10",
-        },
-        {
-          pair: "SOL/USDT",
-          lev: "5X",
-          profit: "-25",
-        },
-        {
-          pair: "CELO/USDT",
-          lev: "5X",
-          profit: "-25",
-        },
-        {
-          pair: "ETH/USDT",
-          lev: "5X",
-          profit: "-25",
-        },
-      ],
-    },
-  ];
 
   const cardsPerPage = 3;
   const [currentPage, setCurrentPage] = useState(1);
-
   const reversedData = [...data].reverse();
-
-  const calculateAverage = (key) => {
-    return reversedData.map((item) => {
-      if (item.trades) {
-        const tradeValues = item.trades.reduce(
-          (tradeAcc, trade) => {
-            tradeAcc.profit += parseInt(trade.profit) || 0;
-            tradeAcc.totalTrades += 1;
-            tradeAcc.winCount += parseInt(trade.profit) > 0 ? 1 : 0;
-            return tradeAcc;
-          },
-          { profit: 0, totalTrades: 0, winCount: 0 }
-        );
-        if (key === "return") {
-          return tradeValues.profit;
-        } else if (key === "accuracy") {
-          return (tradeValues.winCount / tradeValues.totalTrades) * 100 || 0;
-        }
-      }
-      return 0;
-    });
-  };
-
   const totalPages = Math.ceil(reversedData.length / cardsPerPage);
 
   const handleNextPage = () => {
@@ -303,8 +19,29 @@ const Results = () => {
 
   const startIdx = (currentPage - 1) * cardsPerPage;
   const endIdx = startIdx + cardsPerPage;
-
   const visibleData = reversedData.slice(startIdx, endIdx);
+
+  const calculateAverage = (key, trades) => {
+    if (!trades) return 0;
+
+    const tradeValues = trades.reduce(
+      (tradeAcc, trade) => {
+        tradeAcc.profit += parseInt(trade.profit) || 0;
+        tradeAcc.totalTrades += 1;
+        tradeAcc.winCount += parseInt(trade.profit) > 0 ? 1 : 0;
+        return tradeAcc;
+      },
+      { profit: 0, totalTrades: 0, winCount: 0 }
+    );
+
+    if (key === "return") {
+      return tradeValues.profit || 0;
+    } else if (key === "accuracy") {
+      return (tradeValues.winCount / tradeValues.totalTrades) * 100 || 0;
+    }
+
+    return 0;
+  };
   
   return (
     <div className="bg-main bg-cover font-Montserrat">
@@ -316,41 +53,37 @@ const Results = () => {
           Every trade we have every took
         </p>
         <div className="grid grid-cols-1 grid-flow-row gap-4 max-w-3xl mx-auto">
-          {visibleData.map((item, index) => (
-            <div
-              key={item.id}
-              className="p-4 border border-white border-opacity-10 rounded-lg bg-[#fff] bg-opacity-10 backdrop-blur-[20px]"
-            >
-              <div className="mb-4 flex justify-between">
-                <h1 className="text-4xl font-bold text-white">
-                  <span className="text-yellow">{`${item.month}`}</span>{" "}
-                  {`${item.year}`}
-                </h1>
-                <div>
-                  <ul className="text-light-gray text-right">
-                    <li>Signal Gains: {calculateAverage("return")[index].toFixed(0)}%</li>
-                    <li>Accuracy: {calculateAverage("accuracy")[index].toFixed(0)}%</li>
-                  </ul>
-                </div>
-              </div>
-              <div>
+        {visibleData.map((item, index) => (
+        <div key={item.id} className="p-8 border border-white border-opacity-10 rounded-lg bg-[#fff] bg-opacity-10 backdrop-blur-[20px]">
+          <div className="mb-8 flex justify-between">
+            <h1 className="text-4xl font-bold text-white">
+              <span className="text-yellow">{`${item.month}`}</span> {`${item.year}`}
+            </h1>
+            <div>
+              <ul className="text-light-gray text-right">
+                <li>Signal Gains: {calculateAverage("return", item.trades).toFixed(0)}%</li>
+                <li>Accuracy: {calculateAverage("accuracy", item.trades).toFixed(0)}%</li>
+              </ul>
+            </div>
+          </div>
+              <div>                
                 <table className="table-auto w-full">
-                  <thead>
+                  <thead className="text-white">
                     <tr>
-                      <th>No</th>
-                      <th>Trading Pair</th>
-                      <th>Leverage</th>
-                      <th>Profit</th>
+                      <th scope="col">No</th>
+                      <th scope="col">Trading Pair</th>
+                      <th scope="col">Leverage</th>
+                      <th scope="col">PNL</th>
                     </tr>
                   </thead>
-                  <tbody className="text-center">
+                  <tbody className="text-center text-light-gray">
                     {item.trades &&
                       item.trades.map((trade, index) => (
                         <tr key={index}>
-                          <td>{index + 1}</td>
-                          <td>{trade.pair}</td>
-                          <td>{trade.lev}</td>
-                          <td>{trade.profit}%</td>
+                          <td className="py-2">{index + 1}</td>
+                          <td className="py-2">{trade.pair}</td>
+                          <td className="py-2">{trade.lev}</td>
+                          <td className="py-2">{trade.profit}%</td>
                         </tr>
                       ))}
                   </tbody>
@@ -360,7 +93,7 @@ const Results = () => {
           ))}
         </div>
       </div>
-      <div className="flex justify-center items-center mt-4">
+      <div className="flex justify-center items-center mt-4 pb-8">
         <button
           onClick={handlePrevPage}
           disabled={currentPage === 1}
