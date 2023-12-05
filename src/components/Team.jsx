e0f9e193c87560cc8972593497955c4bb86d05c9
Szilárd Máté <mate.szilard.mark@gmail.com>
2023-12-05 18:08:31 +0100

    Updated About

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,68 +1,68 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Team = () => {
+const history = useHistory();
+
   return (
-    <div className="container mx-auto text-center md:flex md:justify-center md:items-center md:p-5 lg:p-3">
-      <div className="inline-block md:max-w-3xl w-full h-auto">
-        <div className="flex justify-evenly">
-          <ProfileDiv
-            imgSrc="/assets/szilard.jpg"
-            altText="Photo of the founder of PARQ Technology"
-            fullName="Szilárd Máté"
-            jobTitle="Chief Executive Officer"
-          />
-          <ProfileDiv
-            imgSrc="/assets/rui.jpg"
-            altText="Photo of the Chief Marketing Officer of PARQ Technology"
-            fullName="Rui Almeida"
-            jobTitle="Chief Marketing Officer"
-          />
+    <div className="container mx-auto grid md:grid-cols-2 gap-8 sm:grid-cols-1">
+      <div className="grid grid-cols-2 grid-rows-2 text-center">
+
+        <img className="rounded-full border-4 border-purple col-start-1 col-span-1 row-start-1 row-span-1 w-full h-auto" src="/assets/szilard.jpg"/>
+
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-2xl text-white font-bold text-center mb-2">
+            Szilárd Máté
+          </p>
+          <p className="text-xl text-light-gray font-medium text-center mb-4">
+            CFO
+          </p>
         </div>
-        <div className="flex justify-around">
-          <ProfileDiv
-            imgSrc="/assets/rahul.jpg"
-            altText="Photo of the Software Engineer of PARQ Technology"
-            fullName="Rahul Kapoor"
-            jobTitle="Software Engineer"
-          />
-          <ProfileDiv
-            imgSrc="/assets/viktor.jpg"
-            altText="Photo of the Operations Manager of PARQ Technology"
-            fullName="Viktor Petrov"
-            jobTitle="Operations Manager"
-          />
+
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-2xl text-white font-bold text-center mb-2">
+            Milan Patrick
+          </p>
+          <p className="text-xl text-light-gray font-medium text-center mb-4">
+            CMO
+          </p>
         </div>
+
+        <img className="rounded-full border-4 border-purple col-start-2 col-span-1 row-start-2 row-span-1 w-full h-auto" src="/assets/milan.jpg"/>
+
       </div>
-      <div className="mt-5">
-        <h1 className="text-green text-left text-4xl font-bold mb-4">Meet The A-Team</h1>
-        <p className="mb-8 text-justify text-light-gray max-w-x">
-          Pioneered by Szilárd, who has dedicated the last two years to curating the finest talents in the industry.
+
+
+      <div>
+        <h1 className="text-yellow text-left text-4xl font-bold mb-4">
+          Meet The A-Team
+        </h1>
+        <p className="mb-8 text-justify text-light-gray max-w-2xl leading-6">
+          The driving force behind our success is our dynamic team, meticulously
+          curated with the industry's finest talent. Committed to creating
+          future-proof and innovative technology, our experts bring unparalleled
+          expertise to the forefront of parking solutions.
         </p>
-        <p className="mb-8 text-justify text-light-gray max-w-x">
-          The driving force behind our success is our dynamic team, meticulously curated with the industry's finest talent. Committed to creating future-proof and innovative technology, our experts bring unparalleled expertise to the forefront of parking solutions.
-        </p>
-        <p className="mb-8 text-justify text-light-gray max-w-xl">
-          Our team isn't just a group of highly skilled individuals, but a collaborative powerhouse united by a shared mission: to revolutionize the parking experience through cutting-edge technology. Each team member, handpicked for their unique expertise and fervent passion for innovation, contributes to a culture of excellence and relentless pursuit of perfection.
+        <p className="mb-8 text-justify text-light-gray max-w-2xl leading-6">
+          Our team isn't just a group of highly skilled individuals, but a
+          collaborative powerhouse united by a shared mission: to revolutionize
+          the parking experience through cutting-edge technology. Each team
+          member, handpicked for their unique expertise and fervent passion for
+          innovation, contributes to a culture of excellence and relentless
+          pursuit of perfection.
         </p>
         <button
-          type="button"
-          className="w-full p-3 text-white font-thin bg-blue-purple hover:bg-purple focus:outline-none rounded-md duration-300 hover:translate-y-[-10px]"
-          onClick={() => window.location.href = '#demo'}
-        >
-          <b className="text-yellow-500">Speak with the Team Today</b>
-          <br />Schedule a free DEMO call today
+            type="button"
+            className="w-full p-3 leading-6 text-white font-thin bg-blue-purple hover:bg-purple focus:outline-none rounded-lg duration-300 hover:translate-y-[-10px]"
+            onClick={() => history.push("/coaching#calendly")}
+    >
+          <b className="text-white font-bold text-xl">
+            Speak with the Team Today
+          </b>
+          <br />
+          Schedule a free DEMO call today
         </button>
       </div>
-    </div>
-  );
-};
-
-const ProfileDiv = ({ imgSrc, altText, fullName, jobTitle }) => {
-  return (
-    <div className="p-4">
-      <img className="rounded-full max-w-[200px] border-4 border-purple mb-4" src={imgSrc} alt={altText} />
-      <p className="text-white font-bold text-center mb-2">{fullName}</p>
-      <p className="text-light-gray font-medium text-center mb-4">{jobTitle}</p>
     </div>
   );
 };
